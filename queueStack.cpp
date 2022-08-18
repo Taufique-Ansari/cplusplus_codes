9a6b58623e140a7a22f4b0b6adada7d6e9ad95d4
Taufique Ansari <taufiqueansari@Taufiques-MacBook-Air.local>
2022-08-19 01:06:25 +0530

    done

---
+++ queueStack.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class que{
+    stack<int> st1;
+    public:
+    void push(int x){
+        st1.push(x);
+    }
+    int pop(){
+        if(st1.empty()){
+            cout<<"Empty Queue"<<endl;
+            return -1;
+        }
+        int x = st1.top();
+        st1.pop();
+        if(st1.empty()){
+            return x;
+        }
+        int item = pop();
+        st1.push(x);
+        return item;
+    }
+    bool empty(){
+        if(st1.empty()){
+            return true;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    que q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    return 0;
+}
